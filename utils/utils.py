d787f961276c8a9f46472580570cf77e7db8c721
mindong <dymin01@naver.com>
2021-09-02 10:38:39 +0900

    #S05P21A105-12 데이터 시각화

--- utils/utils.py
+++ utils/utils.py
@@ -12,3 +12,21 @@ import csv
 def save_config():
 	pass
 
+
+# Req. 4-1	이미지와 캡션 시각화
+def visualize_img_caption(img_paths, caption):
+	caption = open(img_paths + '/captions.csv', 'r', encoding='utf-8')
+	caption_list = list(csv.reader(caption))
+	caption_example = caption_list[2][0].split('|')
+	caption_img_name = caption_example[0]
+	caption_caption = caption_example[2].strip()
+
+	img_path = img_paths + '/' + caption_img_name
+	img = mpimg.imread(img_path)
+	plt.title('<start> ' + caption_caption + ' <end>')
+	plt.imshow(img)
+	plt.show()
+	return
+
+
+
