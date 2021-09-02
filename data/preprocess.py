5efdf98602967dc900a84295835542e16373e7bd
심재우 <jw_shim@naver.com>
2021-09-02 13:42:43 +0900

    #S05P21A105-13 이미지 캡셔닝 데이터 전처리 preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,23 +1,57 @@
 import os
 import csv
 import numpy as np
-
+import pandas as pd
+import pathlib
+import shutil
 
 # Req. 3-1	이미지 경로 및 캡션 불러오기
 def get_path_caption():
-    pass
+    arr = list()
+    arr.append('./datasets/images')
+    path = pathlib.Path('./datasets/captions.csv')
+    caption = pd.read_csv(path, encoding='utf-8', sep='|')
+    arr.append(caption)
+    return
 
 
 # Req. 3-2	전체 데이터셋을 분리해 저장하기
 def dataset_split_save():
-    pass
+    caption = open('./datasets/captions.csv', 'r', encoding='utf-8')
+    caption_list = list(csv.reader(caption))
+    caption_length = len(caption_list)
+    test_length = caption_length * 8 // 10
+    training_list, test_list = caption_list[1:test_length], caption_list[test_length:] #컬럼을 빼주려고 1부터 시작
+    f = open("./datasets/train/captions.csv", "w", encoding="utf-8")
+    f.write('image_name| comment_number| comment\n')
+    for line in training_list:
+        f.write(' '.join(line))
+        f.write('\n')
+        # 이미지파일 복사
+        # img_name = line[0].split('|')[0]
+        # shutil.copy("./datasets/images/" + img_name, './datasets/train/' + img_name)
+        
+    f = open("./datasets/test/captions.csv", "w", encoding="utf-8")
+    f.write('image_name| comment_number| comment\n')
+    for line in test_list:
+        f.write(' '.join(line))
+        f.write('\n')
+        # 이미지파일 복사
+        # img_name = line[0].split('|')[0]
+        # shutil.copy("./datasets/images/" + img_name, './datasets/test/' + img_name)
+    return ['./datasets/train', './datasets/test']
 
 
 # Req. 3-3	저장된 데이터셋 불러오기
-def get_data_file():
-    pass
+def get_data_file(path):
+    arr = list()
+    arr.append(path)
+    path = pathlib.Path(path + '/captions.csv')
+    caption = pd.read_csv(path, encoding='utf-8', sep='|')
+    arr.append(caption)
+    return arr
 
 
 # Req. 3-4	데이터 샘플링
 def sampling_data():
-    pass
+    return [1, 2]
