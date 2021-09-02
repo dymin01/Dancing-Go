3d9aa1e63e9704eda94042a6e1239a355e5ff4bf
mindong <dymin01@naver.com>
2021-09-02 10:37:09 +0900

    #S05P21A105-9 이미지 캡셔닝 데이터 전처리

--- train.py
+++ train.py
@@ -4,25 +4,26 @@ from utils import utils
 
 
 # config 저장
-utils.save_config()
+# utils.save_config()
 
 
 # 이미지 경로 및 캡션 불러오기
-img_paths, captions = preprocess.get_path_caption()
+# img_paths, captions = preprocess.get_path_caption()
+preprocess.get_path_caption()
 
 
-# 전체 데이터셋을 분리해 저장하기
+# 전체 데이터셋을 분리해 저장하기ss
 train_dataset_path, val_dataset_path = preprocess.dataset_split_save()
 
 
 # 저장된 데이터셋 불러오기
-img_paths, caption = preprocess.get_data_file()
+img_paths, caption = preprocess.get_data_file(train_dataset_path)
 
 
 # 데이터 샘플링
-if config.do_sampling:
-    img_paths, caption = preprocess.sampling_data()
+# if config.do_sampling:
+#     img_paths, caption = preprocess.sampling_data()
 
 
 # 이미지와 캡션 시각화 하기
-utils.visualize_img_caption()
+utils.visualize_img_caption(img_paths, caption)
--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 import os
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 import tensorflow as tf
+import csv
 
+# import config from '../config.py'
 
 # Req. 2-2	세팅 값 저장
 def save_config():
 	pass
 
-
-# Req. 4-1	이미지와 캡션 시각화
-def visualize_img_caption():
-	pass
