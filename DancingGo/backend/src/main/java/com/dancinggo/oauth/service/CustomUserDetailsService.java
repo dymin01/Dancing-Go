cb091d7885ad2cf90d0380ee24b6dab30fdc5e70
심재우 <jw_shim@naver.com>
2021-09-20 21:27:53 +0900

    #S05P21A105-456 BE [추가] 소셜로그인 구글, 네이버, 카카오 로그인 구현

---
+++ DancingGo/backend/src/main/java/com/dancinggo/oauth/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.dancinggo.oauth.service;
+
+import com.dancinggo.db.entity.User;
+import com.dancinggo.db.repository.UserRepository;
+import com.dancinggo.oauth.entity.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserId(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Can not find username.");
+        }
+        return UserPrincipal.create(user);
+    }
+}
