e373cc9239ad909cba0a93920772d26c350fc5d3
정은교 <jeeg111@naver.com>
2021-09-21 22:54:07 +0900

    #S05P21A105-481 FE [추가] Home 모드 선택 슬라이드 구현

---
+++ DancingGo/frontend/src/components/home/ModeSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <carousel-3d id="carousel" :width="600" :height="400">
+    <slide :index="0" class="slide">
+        <img src="images/main/main_background.jpg" alt="">
+    </slide>
+    <slide :index="1" class="slide">
+        <img src="images/home/home_background.jpg" alt="">
+    </slide>
+    <slide :index="2" class="slide">
+      <img src="images/home/sample.jpg" alt="">
+    </slide>
+  </carousel-3d>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d'
+export default {
+  name: 'ModeSelect',
+  components: {
+    Carousel3d,
+    Slide,
+  }
+}
+</script>
+
+<style>
+#carousel {
+  position: absolute;
+  bottom: 20vh;
+  overflow: visible;
+}
+.slide {
+  box-shadow: 0 0 20px white;
+  border-radius: 2%;
+}
+</style>
--- DancingGo/frontend/src/main.js
+++ DancingGo/frontend/src/main.js
@@ -7,6 +7,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Carousel3d from 'vue-carousel-3d'
+
+Vue.use(Carousel3d)
 
 Vue.config.productionTip = false
 
