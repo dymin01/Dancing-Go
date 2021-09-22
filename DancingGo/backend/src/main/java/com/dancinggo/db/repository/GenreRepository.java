de990dd751a9505ebd31d2158d3b13e50e5c61e0
심재우 <jw_shim@naver.com>
2021-09-22 23:21:01 +0900

    #S05P21A105-476 BE [추가] 노래 전체 가져오기

---
+++ DancingGo/backend/src/main/java/com/dancinggo/db/repository/GenreRepository.java
@@ -0,0 +1,10 @@
+package com.dancinggo.db.repository;
+
+import com.dancinggo.db.entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    Optional<Genre> findByGenreId(Long genreId);
+}
