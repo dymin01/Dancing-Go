db973cf9a61e7ef61d471c1e03d2bb1f162878c5
심재우 <jw_shim@naver.com>
2021-09-24 16:57:38 +0900

    #S05P21A105-490 FE [추가] 뱃지 획득 모달 구현

--- DancingGo/frontend/src/main.js
+++ DancingGo/frontend/src/main.js
@@ -9,10 +9,17 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import Carousel3d from 'vue-carousel-3d'
 import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Carousel3d)
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 const BACKEND_PORT = process.env.BACKEND_PORT === null ? '' : `:${process.env.BACKEND_PORT}`
 const BACKEND_DOMAIN = process.env.BACKEND_DOMAIN === null ? `${location.protocol}//${location.hostname}` : process.env.BACKEND_DOMAIN
