5cac71ff647f3cad776d3cd9ec8841c5c7f7674a
권순주 <ksj96226@naver.com>
2021-09-24 10:06:08 +0900

    #S05P21A105-461 FE [추가] 명예의 전당 배경 이미지

--- DancingGo/frontend/src/views/HallofFame.vue
+++ DancingGo/frontend/src/views/HallofFame.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>명예의전당</h1>
+    <v-img id="background" src="images/HallofFame/stage3.jpg"></v-img>
+    <v-img id="Firecracker" src="images/HallofFame/Firecracker.png"></v-img>
+    <v-img id="rank" src="images/HallofFame/rank.png"></v-img>
   </div>
 </template>
 
@@ -11,5 +13,34 @@ export default {
 </script>
 
 <style>
+
+html {
+  overflow: hidden;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+html::-webkit-scrollbar {
+  display: none;
+}
+
+#background {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+}
+
+#Firecracker {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+}
+
+#rank {
+  position: absolute;
+  width: 50%;
+  left: 25vw;
+  bottom: 35vh;
+}
 
 </style>
