f7c143674c3925a3d6c636b930f45c6de5343a94
정은교 <jeeg111@naver.com>
2021-09-26 00:52:03 +0900

    #S05P21A105-487 FE [추가] 닉네임 없을 시 로그인 후 홈에서 닉네임 설정 모달 활성화

--- DancingGo/frontend/src/views/Home.vue
+++ DancingGo/frontend/src/views/Home.vue
@@ -4,17 +4,35 @@
     <button @click="logout">로그아웃</button>
     <ModeSelect />
 
+    <!-- <NicknameModal /> -->
+
+    <v-dialog
+    v-model="isNicknameModalOpen"
+    max-width="400px"
+    >
+      <NicknameModal
+      @closeModal="closeNicknameModal" />
+      <!-- <Mypage @closeMypage="closeMypage" class="mypageModal"/> -->
+    </v-dialog>
+
   </div>
 </template>
 
 <script>
 import ModeSelect from '@/components/home/ModeSelect.vue'
+import NicknameModal from '@/components/home/NicknameModal.vue'
 import { mapMutations } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
     ModeSelect,
+    NicknameModal
+  },
+  data () {
+    return {
+      isNicknameModalOpen: false,
+    }
   },
   methods: {
     ...mapMutations(['setToken', 'setUser']),
@@ -23,6 +41,16 @@ export default {
       this.setUser(null)
       alert('로그아웃되었습니다.')
       if (this.$route.path !== '/') this.$router.push('/')
+    },
+    closeNicknameModal () {
+      this.isNicknameModalOpen = false
+    }
+  },
+  created () {
+    // user 닉네임이 없으면 모달 오픈
+    const userNickname = this.$store.state.account.user.userNickname
+    if (!userNickname) {
+      this.isNicknameModalOpen = !this.isNicknameModalOpen
     }
   }
 }
