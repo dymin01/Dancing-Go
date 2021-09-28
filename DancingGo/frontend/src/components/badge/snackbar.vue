3ffb3466e570620b74dd23d9a4c264aa52c97132
심재우 <jw_shim@naver.com>
2021-09-28 14:08:41 +0900

    #S05P21A105-553 FE [추가] v-snackbar로 뱃지 획득 창 vue 추가

---
+++ DancingGo/frontend/src/components/badge/snackbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="text-center">
+    <v-btn
+      dark
+      color="indigo"
+      @click="snackbar = true"
+    >
+    Open Snackbar
+    </v-btn>
+
+    <v-snackbar
+      v-model="snackbar"
+      :vertical="vertical"
+      :height="height"
+      :width="width"
+      :timeout="timeout"
+      centered
+    >
+      <img src="images/badgeImg/1001.png" width="200px">
+      {{ text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="indigo"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return{
+      text: `Hello, I'm a snackbar`,
+      snackbar: false,
+      vertical: true,
+      height: '300px',
+      width: '500px',
+      timeout : 2000,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
