818b342aa82348e41b3a86f89126dc4bfcb395f8
정은교 <jeeg111@naver.com>
2021-09-29 23:39:06 +0900

    BE [수정] Opencv 이식 진행중

--- DancingGo/AI/ai_back/ai_back/settings.py
+++ DancingGo/AI/ai_back/ai_back/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ai_back.urls'
--- DancingGo/AI/ai_back/dancinggo_ai/views.py
+++ DancingGo/AI/ai_back/dancinggo_ai/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from .test import test
 
 # Create your views here.
-@api_view(['GET'])
-def ai(request):
+@api_view(['POST'])
+def upload_image(request):
+   print(request.FILES['image'].file)
+   image_file = request.FILES['image'].file
+   test(image_file)
 
-   return 
+   result_json = { 'result': 'ok' }
+   return JsonResponse(result_json)
