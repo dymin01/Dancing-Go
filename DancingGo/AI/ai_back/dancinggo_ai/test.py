34722101a2d538eeb20ed18139afbe1a02abb754
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-09-30 16:01:59 +0900

    #S05P21A105-564 FE [수정] ai 백엔드 수정
    Former-commit-id: 5f05b1401455881400b44aa70fc9e62477805b85

--- DancingGo/AI/ai_back/dancinggo_ai/test.py
+++ DancingGo/AI/ai_back/dancinggo_ai/test.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import cv2
 import os
 
-def test(testtest):
+def test(imagePath):
     # 관절 번호: 머리는 0, 목은 1 등등
     BODY_PARTS = {"Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
                 "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
@@ -14,61 +14,55 @@ def test(testtest):
                 ["RElbow", "RWrist"], ["Neck", "LShoulder"], ["LShoulder", "LElbow"],
                 ["LElbow", "LWrist"], ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"],
                 ["RKnee", "RAnkle"], ["Chest", "LHip"], ["LHip", "LKnee"], ["LKnee", "LAnkle"]]
-    print('3434')
     # 훈련된 network 세팅
-    # 이부분에서 계속 오류가 난다...django에서 외부 파일 읽어들이는 방법이 뭔지 모르겠음
     # 참고영상: https://www.youtube.com/watch?v=KcsnCGYt928
-    # protoFile = os.path.sep.join([settings.BASE_DIR, "pose_detection_model/pose_deploy_linevec_faster_4_stages.prototxt"])
-    protoFile = "../pose_detection_model/pose_deploy_linevec_faster_4_stages.prototxt"
-    # weightsFile = os.path.sep.join([settings.BASE_DIR, "pose_detection_model/pose_iter_160000.caffemodel"])
-    weightsFile = "../pose_detection_model/pose_iter_160000.caffemodel"
-    # print(type(protoFile))
+    BASE_DIR = 'C:/Users/multicampus/Documents/S05P21A105/DancingGo/AI/ai_back/'
+    # protoFile = "C:/Users/multicampus/Documents/S05P21A105/DancingGo/AI/ai_back/pose_deploy_linevec_faster_4_stages.prototxt"
+    # weightsFile = "C:/Users/multicampus/Documents/S05P21A105/DancingGo/AI/ai_back/pose_iter_160000.caffemodel"
+    protoFile = "C:/Users/multicampus/Documents/S05P21A105/DancingGo/AI/ai_back/pose_deploy_linevec.prototxt"
+    weightsFile = "C:/Users/multicampus/Documents/S05P21A105/DancingGo/AI/ai_back/pose_iter_440000.caffemodel"
     net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
-    print('123')
-
     # # 테스트 이미지 읽기
-    # image = cv2.imread(path)
-    
+    image = cv2.imread(imagePath)
+
     # # 테스트 이미지에서 height, width, color 정보 파악
-    # imageHeight, imageWidth, imageColor = image.shape
+    imageHeight, imageWidth, imageColor = image.shape
+        
+    # 테스트 이미지를 network에 넣기 위해 전처리
+    inpBlob = cv2.dnn.blobFromImage(image, 1.0 / 255, (imageWidth, imageHeight), (0, 0, 0), swapRB=False, crop=False)
+    # 테스트 이미지를 network에 넣어줌
+    net.setInput(inpBlob)
+    print('보냈다')
+    # 결과 받아오기
+    output = net.forward()
+    print('받았다')
+    # print(output)
     
-    # # 테스트 이미지를 network에 넣기 위해 전처리
-    # inpBlob = cv2.dnn.blobFromImage(image, 1.0 / 255, (imageWidth, imageHeight), (0, 0, 0), swapRB=False, crop=False)
+    H = output.shape[2]
+    W = output.shape[3]
     
-    # # 테스트 이미지를 network에 넣어줌
-    # net.setInput(inpBlob)
+    # 검출된 관절 포인트를 테스트 이미지에 그려주기
+    points = []
+    for i in range(0, 15):
+        # 해당 관절 신뢰도 얻기
+        probMap = output[0, i, :, :]
+        # global 최대값 찾기
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        # 원래 이미지에 맞게 점 위치 변경
+        x = (imageWidth * point[0]) / W
+        y = (imageHeight * point[1]) / H
+        # 키포인트 검출한 결과가 0.1보다 크면(검출한곳이 위 BODY_PARTS랑 맞는 부위면) points에 추가, 검출했는데 부위가 없으면 None으로
+        if prob > 0.1:
+            cv2.circle(image, (int(x), int(y)), 3, (0, 255, 255), thickness=-1,
+                    lineType=cv2.FILLED)  # circle(그릴곳, 원의 중심, 반지름, 색)
+            cv2.putText(image, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1,
+                        lineType=cv2.LINE_AA)
+            points.append((int(x), int(y)))
+        else:
+            points.append(None)
     
-    # # 결과 받아오기
-    # output = net.forward()
-    
-    # H = output.shape[2]
-    # W = output.shape[3]
-    
-    # # 검출된 관절 포인트를 테스트 이미지에 그려주기
-    # points = []
-    # for i in range(0, 15):
-    #     # 해당 관절 신뢰도 얻기
-    #     probMap = output[0, i, :, :]
-    
-    #     # global 최대값 찾기
-    #     minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-    
-    #     # 원래 이미지에 맞게 점 위치 변경
-    #     x = (imageWidth * point[0]) / W
-    #     y = (imageHeight * point[1]) / H
-    
-    #     # 키포인트 검출한 결과가 0.1보다 크면(검출한곳이 위 BODY_PARTS랑 맞는 부위면) points에 추가, 검출했는데 부위가 없으면 None으로
-    #     if prob > 0.1:
-    #         cv2.circle(image, (int(x), int(y)), 3, (0, 255, 255), thickness=-1,
-    #                 lineType=cv2.FILLED)  # circle(그릴곳, 원의 중심, 반지름, 색)
-    #         cv2.putText(image, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1,
-    #                     lineType=cv2.LINE_AA)
-    #         points.append((int(x), int(y)))
-    #     else:
-    #         points.append(None)
-    
-    # cv2.imshow("Output-Keypoints", image)
-    # cv2.waitKey(0)
+    cv2.imshow("Output-Keypoints", image)
+    cv2.waitKey(0)
     
     # # 관절들을 선으로 연결해주기
     # for pair in POSE_PAIRS:
