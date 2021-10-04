347805a0f6f51eb37a515a119f8cc4abf45034e5
권순주 <ksj96226@naver.com>
2021-10-04 15:08:22 +0900

    #S05P21A105-684 FE [수정] 랭킹 리스트 4위부터 불러오기 - watch에 추가
    Former-commit-id: 8ad7621ca7509aefff2d98bd2b02aa76c5adb0c3

--- DancingGo/frontend/src/components/musicselect/songRank.vue
+++ DancingGo/frontend/src/components/musicselect/songRank.vue
@@ -134,10 +134,10 @@ export default {
           .get('/score/songRank/'+this.songId)
           .then((res) => {
             console.log(this.songId)
-            this.rankList = res.data;
-            this.firstRank = this.rankList[0]
-            this.secondRank = this.rankList[1]
-            this.thirdRank = this.rankList[2]
+            this.rankList = res.data.slice(3, res.data.length);
+            this.firstRank = res.data[0]
+            this.secondRank = res.data[1]
+            this.thirdRank = res.data[2]
         }),
         axios
           .post('/score/findMyScore/', body)
