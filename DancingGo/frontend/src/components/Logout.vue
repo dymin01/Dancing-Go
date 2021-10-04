852a06eb3da566ff5cea700ecf3dc826227b2b44
정은교 <jeeg111@naver.com>
2021-10-04 22:29:36 +0900

    #S05P21A105-572 FE [추가] 환경설정 및 로그아웃 모달 언어 조건 분기
    Former-commit-id: a204e8d92b1b99bb7ec2bda95fe6f0e65ae9f3d3

--- DancingGo/frontend/src/components/Logout.vue
+++ DancingGo/frontend/src/components/Logout.vue
@@ -1,13 +1,19 @@
 <template>
   <v-card class="text-center" id="logout">
-    <v-card-title class="text-h5 justify-center">
-      로그아웃
+    <v-card-title
+    v-if="this.$store.getters.langMode=='한국어'"
+    class="text-h5 justify-center">
+      알림
+    </v-card-title>
+    <v-card-title v-else class="text-h5 justify-center">
+      Notice
     </v-card-title>
     <hr style="margin: 0px; background: white;">
-    <v-card-text class="d-flex row">
-        <div class="cols-6">
-            <h4 style="color: white;" class="mt-4 mb-5">로그아웃 하시겠습니까?</h4>
-        </div>
+    <v-card-text class="py-2">
+      <h5
+      v-if="this.$store.getters.langMode=='한국어'"
+      style="color: white;" class="mt-4 mb-5">로그아웃 하시겠습니까?</h5>
+      <h5 v-else style="color: white;" class="mt-4 mb-5">Do you want to log out?</h5>
     </v-card-text>
     <v-btn
       id="button"
