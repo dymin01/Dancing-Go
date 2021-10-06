cf8880cd8d575f59cc80621e2ff22e74c30113fd
심재우 <jw_shim@naver.com>
2021-10-06 13:32:30 +0900

    #S05P21A105-578 BE auth redirect 수정
    Former-commit-id: 0d98c35286c85c964750739a941fc7be1d44ad52

--- DancingGo/frontend/config/dev.env.js
+++ DancingGo/frontend/config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  //FRONTEND_PORT: 3000,
+  FRONTEND_PORT: 3000,
   BACKEND_DOMAIN: null,
   BACKEND_PORT: 8080,
 })
