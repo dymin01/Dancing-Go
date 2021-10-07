ee97fab6845f4cced400aebcb676c806d4a245f6
심재우 <jw_shim@naver.com>
2021-10-07 16:48:51 +0900

    hotfix post aws
    Former-commit-id: 48c46e17938e8880adeebec3f14de785188aaa7f

--- DancingGo/frontend/src/views/Ranking2.vue
+++ DancingGo/frontend/src/views/Ranking2.vue
@@ -182,7 +182,7 @@ export default {
       }
       let skeletons = []
       // await axios.post('http://localhost:8000/api/v1/', params)
-      await axios.post('https://70.12.130.110:8000/api/v1/', params)
+      await axios.post('https://3.36.49.201:8000/api/v1/', params)
       .then(function(res) {
         skeletons = res.data.skeletons
       })
